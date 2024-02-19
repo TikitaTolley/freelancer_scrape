9eb7309ad686fa516ab42fa71dc5d9299c11b417
Tikita Peralta <tikitaperalta@gmail.com>
2024-02-19 11:44:24 +0000

    found first class

--- scrape.py
+++ scrape.py
@@ -1,8 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.upwork.com/nx/find-work/best-matches'
+URL = 'https://www.freelancer.co.uk/jobs/?keyword=python'
 req = requests.get(URL)
 soup = BeautifulSoup(req.content, "html.parser")
-results = soup.find_all("a")
-print(results)
+results = soup.find("div", {"class": "pinky-template"})
+#print(results)
+children = results.findChildren()
+for child in children:
+    print(child.prettify())
